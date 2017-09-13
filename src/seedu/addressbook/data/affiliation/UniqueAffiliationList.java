aa5c3a9f61ecd6c49d8b4f4e1ba3d348cad05d43
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 03:08:25 +0800

    UniqueAffiliationList: Add import statements

--- src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
+++ src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
@@ -1,5 +1,18 @@
 package seedu.addressbook.data.affiliation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import seedu.addressbook.common.Utils;
+import seedu.addressbook.data.exception.DuplicateDataException;
 
 public class UniqueAffiliationList {
+    
+
 }
