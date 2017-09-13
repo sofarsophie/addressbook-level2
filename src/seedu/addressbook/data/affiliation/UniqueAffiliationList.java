72c4a859c34c9c1477bc2fb6195a758369d92070
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 03:13:11 +0800

    UniqueAffiliationList: Add remaining methods adapted from UniqueTagList

--- src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
+++ src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
@@ -65,4 +65,80 @@ public class UniqueAffiliationList implements Iterable<Affiliation>{
         internalList.addAll(affiliations);
     }
 
+    /**
+     * Constructs a shallow copy of the given affiliations list.
+     */
+    public UniqueAffiliationList(UniqueAffiliationList source) {
+        internalList.addAll(source.internalList);
+    }
+
+    /**
+     * Returns a new Set that is a deep copy of all affiliations in this list.
+     * This set is mutable and change-insulated against the internal list.
+     */
+    public Set<Affiliation> toSet() {
+        return new HashSet<>(internalList);
+    }
+
+
+    /**
+     * Returns true if the list contains an equivalent affiliation as the given argument.
+     */
+    public boolean contains(Affiliation toCheck) {
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds all the given affiliations to this list.
+     *
+     * @throws DuplicateAffiliationException if the argument affiliations list contains affiliation(s) that already exist in this list.
+     */
+    public void addAll(UniqueAffiliationList affiliations) throws DuplicateAffiliationException {
+        if (!Collections.disjoint(this.internalList, affiliations.internalList)) {
+            throw new DuplicateAffiliationException();
+        }
+        this.internalList.addAll(affiliations.internalList);
+    }
+
+    /**
+     * Adds every affiliation from the argument list that does not yet exist in this list.
+     */
+    public void mergeFrom(UniqueAffiliationList affiliations) {
+        final Set<Affiliation> alreadyInside = this.toSet();
+        for (Affiliation affiliation : affiliations) {
+            if (!alreadyInside.contains(affiliation)) {
+                internalList.add(affiliation);
+            }
+        }
+    }
+
+    /**
+     * Clears all affiliations in list.
+     */
+    public void clear() {
+        internalList.clear();
+    }
+
+    /**
+     * Replaces the Tags in this list with those in the argument tag list.
+     */
+    public void setAffiliations(UniqueAffiliationList replacement) {
+        this.internalList.clear();
+        this.internalList.addAll(replacement.internalList);
+    }
+
+    @Override
+    public Iterator<Affiliation> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniqueAffiliationList // instanceof handles nulls
+                && this.internalList.equals(((UniqueAffiliationList) other).internalList));
+    }
+
+
+
 }
