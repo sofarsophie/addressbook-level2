bfce36d81c69a9fa3f054538a009ede075d64c15
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 03:10:32 +0800

    UniqueAffiliationList: Add constructor, exception, list to class based on UniqueTagList class

--- src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
+++ src/seedu/addressbook/data/affiliation/UniqueAffiliationList.java
@@ -12,7 +12,29 @@ import java.util.Set;
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
 
-public class UniqueAffiliationList {
-    
+/**
+ * A list of affiliations. Does not allow nulls or duplicates.
+ *
+ * @see Affiliation#equals(Object)
+ * @see Utils#elementsAreUnique(Collection)
+ */
+
+public class UniqueAffiliationList implements Iterable<Affiliation>{
+
+    /**
+     * Signals that an operation would have violated the 'no duplicates' property of the list.
+     */
+    public static class DuplicateAffiliationException extends DuplicateDataException {
+        protected DuplicateAffiliationException() {
+            super("Operation would result in duplicates.");
+        }
+    }
+
+    private final List<Affiliation> internalList = new ArrayList<>();
+
+    /**
+     * Constructs an empty AffiliationList.
+     */
+    public UniqueAffiliationList() {}
 
 }
