5596c893c0be41968ac9255a9cea04d42a513fa0
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 03:42:36 +0800

    TypicalPersons: Edit person tests

--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -1,6 +1,8 @@
 package seedu.addressbook.util;
 
 import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.affiliation.Affiliation;
+import seedu.addressbook.data.affiliation.UniqueAffiliationList;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
@@ -20,13 +22,13 @@ public class TypicalPersons {
     public TypicalPersons() {
         try {
             amy = new Person(new Name("Amy Buck"), new Phone("91119111", false), new Email("ab@gmail.com", false),
-                    new Address("1 Clementi Road", false), new UniqueTagList());
+                    new Address("1 Clementi Road", false), new UniqueTagList(), new UniqueAffiliationList());
             bill = new Person(new Name("Bill Clint"), new Phone("92229222", false), new Email("bc@gmail.com", false),
-                    new Address("2 Clementi Road", true), new UniqueTagList());
+                    new Address("2 Clementi Road", true), new UniqueTagList(), new UniqueAffiliationList());
             candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
-                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList());
+                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList(), new UniqueAffiliationList());
             dan = new Person(new Name("Dan Smith"), new Phone("1234556", true), new Email("ss@tt.com", true),
-                    new Address("NUS", true), new UniqueTagList(new Tag("Test")));
+                    new Address("NUS", true), new UniqueTagList(new Tag("Test")), new UniqueAffiliationList(new Affiliation("Google")));
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
