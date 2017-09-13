03831c8c9865ad090167b628260b48c02f6300ad
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 03:06:05 +0800

    Affiliation: Construct attributes and validation method based on Tag.java

--- src/seedu/addressbook/data/affiliation/Affiliation.java
+++ src/seedu/addressbook/data/affiliation/Affiliation.java
@@ -1,4 +1,36 @@
 package seedu.addressbook.data.affiliation;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents an Affiliation in the address book.
+ * Guarantees: immutable
+ */
 public class Affiliation {
+    public static final String MESSAGE_AFFIL_CONSTRAINTS = "Affiliation should be alphanumeric.";
+    public static final String AFFIL_VALIDATION_REGEX = "\\p{Alnum}+";
+
+    public final String affilName;
+
+    /**
+     * Validates given affiliation name.
+     *
+     * @throws IllegalValueException if the given affiliation name string is invalid.
+     */
+    public Affiliation(String institution) throws IllegalValueException {
+        String trimmedName = institution.trim();
+        System.out.println(trimmedName);
+        if (!isValidAffilName(trimmedName)) {
+            throw new IllegalValueException(MESSAGE_AFFIL_CONSTRAINTS);
+        }
+        this.affilName = trimmedName;
+    }
+
+    /**
+     * Returns true if a given string is a valid affiliation name.
+     */
+    public static boolean isValidAffilName(String test) {
+        return test.matches(AFFIL_VALIDATION_REGEX);
+    }
+
 }
