0f470294dd0bfc000861d31256b36b58936975a3
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-14 23:55:32 +0800

    UtilsTest: add isAnyNull() test

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -10,6 +10,27 @@ import org.junit.Test;
 
 public class UtilsTest {
 
+    @Test
+    public void isAnyNull() throws Exception {
+
+        // at least one null item
+        assertNull(null, "abc", "bc", "c", null, "d");
+
+        // all null items
+        assertNull(null, null);
+        assertNull(null, null);
+
+        // non-null items
+        assertNotNull(1, 2, 3);
+        assertNotNull("abc", "ab", "b");
+
+        // no item
+        assertNotNull();
+    }
+
+    public void assertNull(Object... objects) { assertTrue(Utils.isAnyNull(objects)); }
+
+    public void assertNotNull(Object... objects) { assertFalse(Utils.isAnyNull(objects)); }
 
     @Test
     public void elementsAreUnique() throws Exception {
