b549d6f3305e3739e36893c3b9f80fcdeb59ca3b
Se Hyun (Sophie) Park <cappuccino96@naver.com>
2017-09-15 18:07:29 +0800

    Contact: Fix equals method

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -13,8 +13,8 @@ public class Contact {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Phone // instanceof handles nulls
-                && this.value.equals(((Phone) other).value)); // state check
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
     }
 
     @Override
